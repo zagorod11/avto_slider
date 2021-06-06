b97a2c2dbbb570100a7bc2966d1877012caa5a3c
zagorod11 <64003625+zagorod11@users.noreply.github.com>
2021-06-06 10:57:08 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(function(){
+$('.slider').slick({
+	prevArrow:'<button class="slick-arrow slick-next"><img src="../img/vpravo.svg" alt="vpravo">  </button>',
+  nextArrow:'<button class="slick-arrow slick-prev"><img src="../img/vlevo.svg" alt="vlevo">  </button>'
+  })
+});
+
